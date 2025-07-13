15cf542bb29a5888215e75a542f7c30576ba3c4d
Jim Fan <iamjimfan@gmail.com>
2025-07-13 01:47:37 +0100

    Parameterise type of OcpiResponse data

--- src/main/java/org/sdf/jimfan/ocpiclient/model/OcpiResponse.java
+++ src/main/java/org/sdf/jimfan/ocpiclient/model/OcpiResponse.java
@@ -5,11 +5,11 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.Date;
 
-public class OcpiResponse extends JsonFormattable {
+public class OcpiResponse<T> extends JsonFormattable {
 
 	@JsonProperty("data")
 	@JsonIgnoreProperties(ignoreUnknown = true) 
-	private Object data;
+	private T data;
 	
 	@JsonProperty("status_code")
 	private int statusCode;
@@ -21,14 +21,14 @@ public class OcpiResponse extends JsonFormattable {
 	@JsonFormat(pattern="yyyy-MM-dd'T'HH:mm:ss.SSSX")
 	private Date timestamp;
 	
-	public OcpiResponse(Object data, int statusCode, String statusMessage, Date timestamp) {
+	public OcpiResponse(T data, int statusCode, String statusMessage, Date timestamp) {
 		this.data = data;
 		this.statusCode = statusCode;
 		this.statusMessage = statusMessage;
 		this.timestamp = timestamp;
 	}
 	
-	public Object getData() {
+	public T getData() {
 		return data;
 	}
 	
