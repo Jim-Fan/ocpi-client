53779534761a3fff2caf06ba4e9d08f0000b6f46
Jim Fan <iamjimfan@gmail.com>
2025-09-20 22:58:38 +0100

    Per-thread JSON object mapper

--- src/main/java/org/sdf/jimfan/ocpiclient/model/JsonFormattable.java
+++ src/main/java/org/sdf/jimfan/ocpiclient/model/JsonFormattable.java
@@ -1,17 +1,28 @@
 package org.sdf.jimfan.ocpiclient.model;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public abstract class JsonFormattable {
 
-	private static ObjectMapper mapper = new ObjectMapper();
+	// This variable is accessed by _all_ instances of _all_ sub-classes, and
+	// can be bad for performance.
+	// private static ObjectMapper mapper = new ObjectMapper();
+	
+	// https://www.baeldung.com/java-threadlocal
+	private static final ThreadLocal<ObjectMapper> mapperThreadLocal =
+			ThreadLocal.withInitial(() -> {
+				ObjectMapper mapper = new ObjectMapper();
+				mapper.registerModule(new JavaTimeModule());
+				return mapper;
+			});
 	
 	@Override
 	public String toString() {
 		String result = null;
 		try {
-			result = mapper.writeValueAsString(this);
+			result = mapperThreadLocal.get().writeValueAsString(this);
 		}
 		catch (JsonProcessingException ex) {
 			result = super.toString();
